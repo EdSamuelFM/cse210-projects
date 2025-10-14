a57d28099a7543c1227d7b5527d034682d61466a
EdSamuelFM <72708772+EdSamuelFM@users.noreply.github.com>
2025-10-14 11:01:53 -0300

    BreathingActivity.cs

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,5 +1,3 @@
-// BreathingActivity.cs
-
 public class BreathingActivity : Activity
 {
     public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
@@ -28,4 +26,5 @@ public class BreathingActivity : Activity
 
         DisplayEndingMessage();
     }
+
 }
